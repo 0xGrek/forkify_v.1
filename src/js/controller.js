3f950f85561720bd0bdbf6960ae7f23959008d49
Serhii <rfrf198231@gmail.com>
2022-12-19 11:16:45 +0000

    add-new-recipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,6 +4,7 @@ import searchView from "./views/searchView";
 import resultsView from "./views/resultsView";
 import paginationView from "./views/paginationView";
 import bookmarksView from "./views/bookmarksView";
+import addRecipeView from "./views/addRecipeView";
 
 import "core-js/stable";
 import "regenerator-runtime/runtime";
@@ -92,6 +93,11 @@ const controlBookmarks = function () {
     bookmarksView.render(model.state.bookmarks);
 };
 
+const controlAddRecipe = function (newRecipe) {
+    console.log(newRecipe);
+    // Upload the new recipe data
+};
+
 controlSearchResults();
 const init = function () {
     bookmarksView.addHandlerRender(controlBookmarks);
@@ -100,5 +106,6 @@ const init = function () {
     recipeView.addHandlerAddBookmark(controlAddBookmark);
     searchView.addHandlerSearch(controlSearchResults);
     paginationView.addHandlerClick(controlPagination);
+    addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 init();
